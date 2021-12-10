74bfdbcd8376a2fac9cfd62a97b6cc8e7a6e4369
Wade Helquist <ZWhiteShadow@Yahoo.com>
2021-12-09 22:11:41 -0700

    Now works correct
    fixed order of high scores, now works with duplicate numbers

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
+
+////        --Comment this out to test -- START -- //
+
         System.out.println("Enter in names and scores of players to see high score list: \n");
         Scanner reader = new Scanner(System.in);
 
@@ -12,47 +15,78 @@ public class Main {
         System.out.println();
 
         int[] scoresArray = new int[numberOfPlayers];
-        String[] playersArray = new String[numberOfPlayers];
+        String[] unsortedPlayersArray = new String[numberOfPlayers];
 
         for (int player = 0; player < numberOfPlayers; player++) {
 
-            System.out.println("Player " + (player + 1) + " of " + numberOfPlayers + ": ");
 
-            System.out.print("Name: ");
-            String name = reader.next();
-            playersArray[player] = name;
+                System.out.println("Player " + (player + 1) + " of " + numberOfPlayers + ": ");
 
-            System.out.print("Score: ");
-            int score = reader.nextInt();
-            scoresArray[player] = score;
+                System.out.print("Name: ");
+                String name = reader.next();
+                unsortedPlayersArray[player] = name;
 
-            System.out.println();
+                System.out.print("Score: ");
+                int score = reader.nextInt();
+                scoresArray[player] = score;
+
+                System.out.println();
+            }
 
         }
 
         reader.close();
 
+//--Comment this out to test -- END -- //
+
+//        // -- Used For Testing -- //
+//        int numberOfPlayers = 9;
+//        int[] scoresArray = {                 7332,         1,       654,        37,    100000,       754,      9874,         0,       5000};
+//        String[] unsortedPlayersArray = {"Player1", "Player2", "Player3", "Player4", "Player5", "Player6", "Player7", "Player8", "Player9",};
+//        //-
+
+//        // -- Used For Testing -- //
+//        int numberOfPlayers = 4;
+//        int[] scoresArray = {                   39,        38,        38,        37};
+//        String[] unsortedPlayersArray = {    "Bob",    "Wade",     "Max",  "Marry"};
+//        //-
+
+
         int[] unsortedScoresArray = scoresArray.clone();
         Arrays.sort(scoresArray);
+        int[] newScoresArray = scoresArray.clone(); //Created due to duplicates
 
-        String[] sortedPlayerArray =  new String[playersArray.length];
+        String[] sortedPlayerArray = new String[numberOfPlayers];
 
-        for (int i = 0; i < scoresArray.length; i++){
-            sortedPlayerArray[i] = playersArray[findIndex(scoresArray,unsortedScoresArray[i])];
+        for (int i = 0; i < numberOfPlayers; i++) {
+            for (int j = 0; j < numberOfPlayers; j++) {
+                if ( unsortedScoresArray[i] == scoresArray[j] ) {
+                    sortedPlayerArray[j] = unsortedPlayersArray[i];
+                    unsortedScoresArray[i] = -1;  //solves duplicate issues
+                    scoresArray[j] = -2; //solves duplicate issues
+                }
+            }
         }
 
         System.out.println("High Score List: ");
         int scoreRank = 0;
-        for (int player = numberOfPlayers - 1; player >= 0 ; player--) {
-            scoreRank++;
-            System.out.println( scoreRank + ": " + sortedPlayerArray[player] + " " + scoresArray[player]);
-        }
 
+        for (int player = numberOfPlayers - 1; player >=0; player--){
+            scoreRank++;
+            System.out.println(scoreRank + ": " + sortedPlayerArray[player] + " " + newScoresArray[player]);
+
+            //If a player next player is the same don't count up
+            try {
+                if ( (newScoresArray[player] == newScoresArray[player - 1] ) ) {
+                    scoreRank--;
+                }
+            }
+            catch(Exception e) {
+
+            }
     }
-    public static int findIndex(int[] arr, int t) // https://www.geeksforgeeks.org/find-the-index-of-an-array-element-in-java/
-    {
-        int index = Arrays.binarySearch(arr, t);
-        return (index < 0) ? -1 : index;
-    }
+
+}
+
 
 }
